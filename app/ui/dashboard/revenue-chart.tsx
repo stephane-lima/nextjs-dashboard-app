a309f5703d38c397b5d2888760b280602dfd674a
stephane-lima <ggabriellopess99999@gmail.com>
2024-05-08 18:00:07 -0100

    Fetching data for <RevenueChart />

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,7 +2,6 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
-// import { fetchRevenue } from '@/app/lib/data';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -15,9 +14,6 @@ export default async function RevenueChart({
 }: {
   revenue: Revenue[];
 }) {
-// }) {
-//   let revenue = await fetchRevenue()
-
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
 
