2ee3ab20899f3916d77d5a0638c4f527e4df2c38
stephane-lima <ggabriellopess99999@gmail.com>
2024-05-08 18:52:28 -0100

    Fetch data

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -43,12 +43,12 @@ export default async function RevenueChart({
 
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
-              <div
+              {/* <div
                 className="w-full rounded-md bg-blue-300"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
-              ></div>
+              ></div> */}
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                 {month.month}
               </p>
