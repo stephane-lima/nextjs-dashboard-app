c57573577889570fa21376421ec40f9b3daf0a21
stephane-lima <ggabriellopess99999@gmail.com>
2024-05-20 15:27:29 -0100

    Fix Login Page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,7 +8,7 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/a
 export default async function Page() {
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            <h1 className={`mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -55,8 +55,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={`truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
